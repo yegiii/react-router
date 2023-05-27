efc0a9e2e5955662b5b484971998c3513f10aa75
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2023-05-27 16:35:14 +0330

    style added

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Classes from "./mainHeader.module.css";
+
 function MainHeader() {
   return (
-    <header>
+    <header className={Classes.header}>
       <nav>
         <ul>
           <li>
