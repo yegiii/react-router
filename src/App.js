e3b18159b15cb82a853ab208be75701a81e50344
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2023-06-06 11:36:30 +0330

    used switch for route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -10,15 +10,17 @@ function App() {
       <MainHeader />
 
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/product-detailes/:productId">
-          <ProductDeatils />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDeatils />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 function Products() {
   return (
     <section>
       <h1>Products page</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 1</li>
-        <li>Product 1</li>
+        <li>
+          <Link to="/products/p1">Product 1</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Product 2</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Product 3</Link>
+        </li>
       </ul>
     </section>
   );
